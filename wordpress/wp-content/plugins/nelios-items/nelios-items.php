0952c7ee79492f6c7952eb7ecca5bfbc421a0b91
dantoulis <google_for_dantoulis@outlook.com>
2026-05-09 15:18:39 +0300

    Add custom WordPress plugin initial state

---
+++ wordpress/wp-content/plugins/nelios-items/nelios-items.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Nelios Items
+ * Description: Headless WordPress item functionality for the Nelios.
+ * Version: 1.0.0
+ * Author: Konstantinos
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
